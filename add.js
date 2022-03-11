44e41eccb2177a47f5ca90ae0cce11f2f39aef8b
olaszadam <71069922+olaszadam@users.noreply.github.com>
2022-03-11 10:31:14 +0100

    Update add.js

--- add.js
+++ add.js
@@ -2,9 +2,9 @@ function sendPost(){
     const data = JSON.stringify({
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        address:document.getElementById("book").value
+        book:document.getElementById("book").value
       });
-      
+
       navigator.sendBeacon('http://127.0.0.1:5000/savedetails/', data);
       console.log(data);
     }
